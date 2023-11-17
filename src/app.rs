64c0be312bb94d12399a067290c07e401b54e5a6
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-11-16 19:01:27 -0500

    remove refcells from data and dom fields

--- src/app.rs
+++ src/app.rs
@@ -7,26 +7,26 @@ pub(crate) mod vdom;
 #[macro_use]
 pub(crate) mod widgets;
 
-use std::{cell::RefCell, mem::ManuallyDrop, rc::Rc};
+use std::{cell::RefCell, rc::Rc};
 
 use wasm_bindgen::JsValue;
 
 use crate::app::widgets::Widget;
 
 struct App<Data, DataAsWidget: Widget<Data>, ToWidget: Fn(&Data) -> DataAsWidget> {
-    data: Rc<RefCell<Data>>,
-    dom: Rc<RefCell<dom::Dom>>,
+    data: Data,
+    dom: dom::Dom,
     to_widget: ToWidget,
 }
 
 impl<Data: 'static, DataAsWidget: Widget<Data> + 'static, ToWidget: Fn(&Data) -> DataAsWidget + Copy + 'static> App<Data, DataAsWidget, ToWidget> {
     fn run(data: Data, to_widget: ToWidget) {
-        let data = Rc::new(RefCell::new(data));
+        let data = data;
 
         let document = web_sys::window().expect("no global window").document().expect("no document on window");
         let app_div = document.get_element_by_id("app").expect("no div with id 'app'");
 
-        let dom = Rc::new(RefCell::new(dom::Dom::new_empty(document, &app_div)));
+        let dom = dom::Dom::new_empty(document, &app_div);
 
         let app = Rc::new(RefCell::new(App { data, dom, to_widget }));
 
@@ -34,19 +34,19 @@ impl<Data: 'static, DataAsWidget: Widget<Data> + 'static, ToWidget: Fn(&Data) ->
     }
 
     fn run_update(app_refcell: &Rc<RefCell<Self>>, event: JsValue, closure: &dyn Fn(JsValue, &mut Data)) {
-        closure(event, &mut app_refcell.borrow().data.borrow_mut());
+        closure(event, &mut app_refcell.borrow_mut().data);
         App::update_dom(app_refcell);
     }
     fn update_dom(app_refcell: &Rc<RefCell<Self>>) {
-        let app = app_refcell.borrow();
+        let mut app = app_refcell.borrow_mut();
         let vdom = dom::vdom_with_closures::Element::from_normal_vdom(
             {
                 let app_refcell = Rc::clone(app_refcell);
                 move |event, update_closure| App::run_update(&app_refcell, event, update_closure)
             },
-            (app.to_widget)(&app.data.borrow()).to_vdom(),
+            (app.to_widget)(&app.data).to_vdom(),
         );
-        app.dom.borrow_mut().update(vdom);
+        app.dom.update(vdom);
     }
 }
 
